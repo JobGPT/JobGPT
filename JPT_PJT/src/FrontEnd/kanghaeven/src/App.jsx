52a602608d6db92ee7a7fdca9f39756788d7c420
강해빈 <meerhae@gmail.com>
2023-06-21 19:20:44 +0900

    update : router

--- JPT_PJT/src/FrontEnd/kanghaeven/src/App.jsx
+++ JPT_PJT/src/FrontEnd/kanghaeven/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css';
-import Login from './components/LogIn';
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import React from 'react';
 
 import HomePage from './components/HomePage';
@@ -10,11 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
-          
-        </Routes>
+        <HomePage />          
       </div>
     </BrowserRouter>
   );
--- JPT_PJT/src/FrontEnd/kanghaeven/src/routes/RouterView.jsx
+++ JPT_PJT/src/FrontEnd/kanghaeven/src/routes/RouterView.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import MainPage from './MainPage';
+import Login from './LogIn';
 
 export default function RouterView() {
   return (
     <Routes>
       <Route path={'/'} element={<MainPage />} />
+      <Route path={'/login'} element={<Login />} />
+      
     </Routes>
   );
 }
