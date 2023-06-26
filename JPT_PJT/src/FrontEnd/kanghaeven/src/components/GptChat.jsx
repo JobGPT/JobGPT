779952a387e755b0e2de46c0c92ff19ce75e37ff
윤태웅 <omygog@naver.com>
2023-06-27 02:13:45 +0900

    feat : kakaomap

--- JPT_PJT/src/FrontEnd/kanghaeven/src/components/GptChat.jsx
+++ JPT_PJT/src/FrontEnd/kanghaeven/src/components/GptChat.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useStore } from '../store.js';
 
+import KaKaoMap from './KaKaoMap.jsx';
 
 import './GptChat.css';
 
-export default function GptChat({msg}) {  
+export default function GptChat({ msg }) {
   const company_info = useStore((store) => store.company_info);
   const current_msg = msg.split(/은|는|이|가|을|를|에/)[0];
   const renderObject = (obj) => {
@@ -12,54 +13,60 @@ export default function GptChat({msg}) {
       const companyInfo = obj.summery.company_overview['table-1'];
       const jobPostings = obj.summery.job_Posting['table-0'];
       const des = companyInfo.description.replace(/\n/g, '<br>');
-      const comp_page = 'https://' + companyInfo['sameAs']
-    if(current_msg == obj.summery.company_overview['table-1'].name){
+      const comp_page = 'https://' + companyInfo['sameAs'];
+      if (current_msg == obj.summery.company_overview['table-1'].name) {
         return (
           <div>
-            <div style={{display: 'flex'}}>
+            <div style={{ display: 'flex' }}>
               <img src={companyInfo.logo} />
               <h3>{companyInfo.name}</h3>
-              <span>{companyInfo.makesOffer }</span>
-              <a href={comp_page} target='_blank'>{companyInfo['sameAs']}</a>
-            </div> {/* 정리 필요 */}
+              <span>{companyInfo.makesOffer}</span>
+              <a href={comp_page} target="_blank">
+                {companyInfo['sameAs']}
+              </a>
+            </div>{' '}
+            {/* 정리 필요 */}
             <table id="info_container">
               <tbody>
-              <tr>
-                <td style={{ backgroundColor: 'lightgray'}}>
-                  기업명
-                </td>
-                <td>{companyInfo.name}</td>
-                <td style={{ backgroundColor: 'lightgray'}}>
-                설립일
-                </td>
-                <td>{obj.summery.company_overview['table-0']['설립일']}</td>
-              </tr>
-              <tr>
-                <td style={{ backgroundColor: 'lightgray'}}>
-                대표자
-                </td>
-                <td>{obj.summery.company_overview['table-0']['대표자']}</td>
-                <td style={{ backgroundColor: 'lightgray'}}>
-                  사원 수
-                </td>
-                <td>{companyInfo.numberOfEmployees}명</td>
-              </tr>
-              <tr>
-                <td style={{ backgroundColor: 'lightgray'}}>
-                기업규모
-                </td>
-                <td>{obj.summery.company_overview['table-0']['기업규모']}</td>
-                <td style={{ backgroundColor: 'lightgray'}}>채용공고 수</td>
-                <td>{Object.keys(jobPostings).length}</td>
-              </tr>
-              </ tbody>
+                <tr>
+                  <td style={{ backgroundColor: 'lightgray' }}>기업명</td>
+                  <td>{companyInfo.name}</td>
+                  <td style={{ backgroundColor: 'lightgray' }}>설립일</td>
+                  <td>{obj.summery.company_overview['table-0']['설립일']}</td>
+                </tr>
+                <tr>
+                  <td style={{ backgroundColor: 'lightgray' }}>대표자</td>
+                  <td>{obj.summery.company_overview['table-0']['대표자']}</td>
+                  <td style={{ backgroundColor: 'lightgray' }}>사원 수</td>
+                  <td>{companyInfo.numberOfEmployees}명</td>
+                </tr>
+                <tr>
+                  <td style={{ backgroundColor: 'lightgray' }}>기업규모</td>
+                  <td>{obj.summery.company_overview['table-0']['기업규모']}</td>
+                  <td style={{ backgroundColor: 'lightgray' }}>채용공고 수</td>
+                  <td>{Object.keys(jobPostings).length}</td>
+                </tr>
+              </tbody>
             </table>
             <br />
             <div id="description_box">
-            <span style={{ fontWeight: 'bold', padding: '5px'}}>{companyInfo.name}?</span>
-            <div dangerouslySetInnerHTML={{ __html: des }} id="description"></div>
+              <span style={{ fontWeight: 'bold', padding: '5px' }}>
+                {companyInfo.name}?
+              </span>
+              <div dangerouslySetInnerHTML={{ __html: des }} id="description"></div>
             </div>
-            <div>{companyInfo['address']}</div>
+            <br />
+            <div id="corp_loc">
+              <div id="top">
+                <h3 id="top_name">회사위치</h3>
+                <p id="address">{companyInfo['address']}</p>
+              </div>
+              <KaKaoMap
+                address={companyInfo['address']}
+                name={companyInfo.name}
+                id="map"
+              />
+            </div>
           </div>
         );
       } else if (current_msg == '채용공고') {
@@ -68,16 +75,17 @@ export default function GptChat({msg}) {
             {Object.entries(jobPostings).map(([key, value]) => (
               <div key={key}>
                 {value[0]}
-                <a href={value.link} target="_blank">{value.link}</a>
+                <a href={value.link} target="_blank">
+                  {value.link}
+                </a>
               </div>
             ))}
           </div>
         );
       } else {
-        return('Chat GPT 연결 예정')
+        return 'Chat GPT 연결 예정';
       }
     }
-    
   };
   return (
     <div id="container">
