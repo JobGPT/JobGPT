310853b02dcdd7f8a0bb984d8716e0d6e732832f
강해빈 <meerhae@gmail.com>
2023-06-26 19:06:41 +0900

    feature : social login 버튼 구현

--- JPT_PJT/src/FrontEnd/kanghaeven/src/routes/AuthPage.jsx
+++ JPT_PJT/src/FrontEnd/kanghaeven/src/routes/AuthPage.jsx
@@ -1,5 +1,8 @@
 import './AuthPage.css'
 import logo from '../assets/logo.svg'
+import google from '../assets/google.png'
+import naver from '../assets/naver.png'
+import kakao from '../assets/kakao.png'
 import { Button } from "react-bootstrap"
 import { Link } from "react-router-dom";
 
@@ -10,15 +13,34 @@ export default function AuthPage() {
       <div className="auth">
         <img src={logo} alt="" className='logoimg' />
         <div className="text">Welcome to JobGPT</div>
-        <div className="text">Log in with your OpenAI account to continue</div>
+        <div className="text">Log in with your JobGPT account to continue</div>
         <div className="buttons">
           <Link to="/login" className='button'>
             <Button className="buttonlogin">Log in</Button>
           </Link>
           <Link to="/signup" className='button'>
-            <Button>Sign up</Button>  
+            <Button className="buttonsignup">Sign up</Button>  
           </Link>
         </div>
+        <div>or</div>
+        <Button variant="outline-primary" className="socialbutton">
+          <div className="social">
+            <img src={google} className="socialimg googleimg"/>
+            <div>Continue with Google</div>
+          </div>
+        </Button>
+        <Button variant="outline-primary" className="socialbutton">
+          <div className="social">
+            <img src={naver} className="socialimg naverimg"/>
+            <div>Continue with Naver</div>
+          </div>
+        </Button>
+        <Button variant="outline-primary" className="socialbutton">
+          <div className="social">
+            <img src={kakao} className="socialimg kakaoimg"/>
+            <div>Continue with Kakao</div>
+          </div>
+        </Button>
       </div>
     </div>
   )
