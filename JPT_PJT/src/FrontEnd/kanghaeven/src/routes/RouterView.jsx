095e5e63792fa06f646965c36f5b287ae39d05ca
kanghaeven <aubrienid@naver.com>
2023-06-28 18:43:26 +0900

    fix : 소셜 로그인 구글 라우트 추가

--- JPT_PJT/src/FrontEnd/kanghaeven/src/routes/RouterView.jsx
+++ JPT_PJT/src/FrontEnd/kanghaeven/src/routes/RouterView.jsx
@@ -5,6 +5,7 @@ import AuthPage from './AuthPage';
 import LogIn from './LogIn';
 import KakaoLogIn from './KakaoLogIn';
 import NaverLogIn from './NaverLogIn';
+import GoogleLogIn from './GoogleLogIn';
 import SignUp from './SignUp';
 import MainPage from './MainPage';
 import ChatPage from './ChatPage';
@@ -16,6 +17,7 @@ export default function RouterView() {
       <Route path={'/login'} element={<LogIn />} />
       <Route path={'/kakaoLogin'} element={<KakaoLogIn />} />
       <Route path={'/naverLogin'} element={<NaverLogIn />} />
+      <Route path={'/googleLogin'} element={<GoogleLogIn />} />
       <Route path={'/signup'} element={<SignUp />} />
       <Route path={'/mainpage'} exact element={<MainPage/>} />
       <Route path={'/mainpage/:index'} exact element={<ChatPage/>} />
